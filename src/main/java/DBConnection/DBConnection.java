96bf4f6f9410b49b2d3567e46353ebb435db96f6
Juan Ferro <juanignacioferro_07@hotmail.com>
2021-08-11 11:25:45 -0300

    agrego singleton a la base de datos

--- src/main/java/DBConnection/DBConnection.java
+++ src/main/java/DBConnection/DBConnection.java
@@ -2,40 +2,26 @@ package DBConnection;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
 
 public class DBConnection {
 
-    Connection con;
-    public static void main(String[] args) {/*puse un main aca solo para testear esta shit*/
-        DBConnection cn = new DBConnection();
-        Statement st;
-        ResultSet rs;
+    private Connection con = null;
 
-        try{
-        } catch (Exception e){
-
-        }
-
-        try {
-            st=cn.con.createStatement();
-            rs=st.executeQuery("select * from TEST1");
-            while (rs.next()) {
-                System.out.println(rs.getString("id")+" " +rs.getString("Nombre")+" " +rs.getString("Contraseña"));
-            }
-            cn.con.close();
-        } catch (Exception e) {
-        }
-
-    }
-    public DBConnection(){
+    private void DBConnection1(){
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             con= DriverManager.getConnection("jdbc:mysql://db4free.net:3306/tpgrp9dds2021","tpgrp9dds2021","38b7315b");
+            System.out.println("Conexion exitosa");
         } catch (Exception e) {
-            System.err.println("Error:" +e);
+            System.err.println("Error en la conexion a la Base de Datos:" +e);
         }
+    }
+
+    public Connection getConexion(){
+        if (con == null){
+            DBConnection1();
+        }
+        return con;
     }
 }
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,29 @@
+import DBConnection.DBConnection;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class Main { /* pa probar cosas*/
-
     public static void main(String[] args) {
+        DBConnection cn = new DBConnection();
+        Statement st;
+        ResultSet rs;
+        Connection DB = (Connection) cn.getConexion();
 
+        try {
+            st=DB.createStatement();
+            rs=st.executeQuery("select * from TEST1");
+            while (rs.next()) {
+                System.out.println(rs.getString("id")+" " +rs.getString("Nombre")+" " +rs.getString("Contraseña"));
+            }
+            DB.close();
+        } catch (Exception e) {
+        }
     }
+
+
+
+
+
 }
